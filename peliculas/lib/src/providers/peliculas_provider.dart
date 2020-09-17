43e0f38ecfa4a80ac5c2139118fabeb5dac9e8de
Marioagtzc <marioagtzc@hotmail.com>
2020-09-17 17:01:37 -0500

    App peliculas

---
+++ peliculas/lib/src/providers/peliculas_provider.dart
@@ -0,0 +1,38 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+import 'package:peliculas/src/models/pelicula_model.dart';
+
+class PeliculasProvider {
+  String _apikey = 'd5f01e15de889615eccf6f397efd353c';
+  String _url = 'api.themoviedb.org';
+  String _language = 'es-ES';
+
+  Future<List<Pelicula>> _procesarRespuesta(Uri url) async {
+    final resp = await http.get(url);
+    final decodedData = json.decode(resp.body);
+
+    final peliculas = new Peliculas.fromJsonList(decodedData['results']);
+
+    return peliculas.items;
+  }
+
+  Future<List<Pelicula>> getEnCines() async {
+    final url = Uri.https(_url, '3/movie/now_playing', {
+      'api_key' : _apikey,
+      'language' : _language,
+    });
+
+    return await _procesarRespuesta(url);
+  }
+  
+  Future<List<Pelicula>> getPopular() async {
+    final url = Uri.https(_url, '3/movie/popular', {
+      'api_key' : _apikey,
+      'language' : _language,
+    });
+
+    return await _procesarRespuesta(url);
+  }
+}
